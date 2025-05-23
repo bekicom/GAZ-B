4f14d9c764a5aa1829f04a7ee168ce575eb351ab
Microsoft N <adelkauz6@gmail.com>
2025-05-23 12:52:27 +0500

    hhh

--- models/Product.js
+++ models/Product.js
@@ -4,13 +4,11 @@ const productSchema = new mongoose.Schema({
   product_name: { type: String, required: true },
   model: { type: String, required: true },
   stock: { type: Number, required: true },
-  product_category: { type: String, required: true },
   purchase_price: { type: Number, required: true },
   sell_price: { type: Number, required: true },
   brand_name: { type: String },
   storeProduct: { type: Boolean, default: false },
   count_type: { type: String, required: true },
- 
   barcode: { type: String, required: true, unique: true },
   special_notes: { type: String },
   kimdan_kelgan: { type: String, required: true },

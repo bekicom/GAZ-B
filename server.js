0f245e22a3fbf84a06fa95c115da0429aa2ec8e1
Microsoft N <adelkauz6@gmail.com>
2025-06-26 09:53:18 +0500

    start

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const corsOptions = {
     "https://gaz-f.vercel.app",
     "http://localhost:3000", // Backend lokal
     "http://localhost:3001", // Backend lokal
+    "http://localhost:3002", // Backend lokal
   ],
   methods: ["GET", "POST", "PUT", "DELETE"], // Ruxsat etilgan HTTP metodlar
   credentials: true, // Cookie va autentifikatsiya uchun ruxsat

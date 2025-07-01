c29c1f9966e2ee610fff1534fc98f78b4ad832eb
bekicom <adelkauz6@gmail.com>
2025-07-01 15:43:31 +0500

    hh

--- models/Debtor.js
+++ models/Debtor.js
@@ -5,9 +5,9 @@ const debtorSchema = new mongoose.Schema(
     name: { type: String, required: true },
     phone: { type: String, required: true },
     debt_amount: { type: Number, required: true },
-    due_date: { type: Date, required:false },
+    due_date: { type: Date, required: false }, // Qarzdor uchun umumiy muddati
     currency: { type: String, enum: ["sum", "usd"], required: true },
-    
+
     payment_log: {
       type: [
         {
@@ -30,7 +30,11 @@ const debtorSchema = new mongoose.Schema(
         product_quantity: { type: Number, required: true },
         sell_price: { type: Number, required: true },
         sold_date: { type: Date, default: Date.now },
-        due_date: { type: Date, required: true },
+
+        // ❌ mana bu noto'g'ri bo'lgan:
+        // due_date: { type: Date, required: true },
+
+        // ✅ to'g'risi: bu maydon kerak emas
       },
     ],
   },
